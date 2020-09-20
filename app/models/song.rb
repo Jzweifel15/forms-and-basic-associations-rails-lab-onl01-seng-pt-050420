71f32921cfca1404bf560c9cd46557d08f121759
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-09-20 20:19:06 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,30 +1,31 @@
 class Song < ActiveRecord::Base
-  belongs_to :artist 
-  belongs_to :genre 
-  has_many :notes 
-  
+  # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
   def artist_name
-    self.artist.name if self.artist 
+    self.artist.name if self.artist
   end
-  
+
   def artist_name=(name)
     self.artist = Artist.find_or_create_by(name: name)
-    self.save 
+    self.save
   end
-  
+
   def genre_name
     self.genre.name if self.genre 
   end
-  
+
   def genre_name=(name)
     self.genre = Genre.find_or_create_by(name: name)
     self.save
   end
-  
+
   def note_contents
     self.notes.collect(&:content)
   end
-  
+
   def note_contents=(contents)
     contents.each do |content|
       unless content.empty?
@@ -33,5 +34,5 @@ class Song < ActiveRecord::Base
       end
     end
   end
-  
+
 end
